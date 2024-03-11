48d0257a12b59a406ad2d6ddcf0d23d9b1927241
alfantasy <71088194+alfantasy@users.noreply.github.com>
2024-03-11 19:16:55 +0400

    lib for Mobile AT

---
+++ libsfor.lua
@@ -0,0 +1,119 @@
+local encoding = require 'encoding' -- регистрация кодирования 
+u8 = encoding.UTF8 -- определение стандартной кодировки
+encoding.default = 'CP1251' -- изменение кодировки на Cyrillic
+
+local functions = {} -- блок экспортируемых функций
+
+local russian_characters = {
+    [168] = 'Ё', [184] = 'ё', [192] = 'А', [193] = 'Б', [194] = 'В', [195] = 'Г', [196] = 'Д', [197] = 'Е', [198] = 'Ж', [199] = 'З', [200] = 'И', [201] = 'Й', [202] = 'К', [203] = 'Л', [204] = 'М', [205] = 'Н', [206] = 'О', [207] = 'П', [208] = 'Р', [209] = 'С', [210] = 'Т', [211] = 'У', [212] = 'Ф', [213] = 'Х', [214] = 'Ц', [215] = 'Ч', [216] = 'Ш', [217] = 'Щ', [218] = 'Ъ', [219] = 'Ы', [220] = 'Ь', [221] = 'Э', [222] = 'Ю', [223] = 'Я', [224] = 'а', [225] = 'б', [226] = 'в', [227] = 'г', [228] = 'д', [229] = 'е', [230] = 'ж', [231] = 'з', [232] = 'и', [233] = 'й', [234] = 'к', [235] = 'л', [236] = 'м', [237] = 'н', [238] = 'о', [239] = 'п', [240] = 'р', [241] = 'с', [242] = 'т', [243] = 'у', [244] = 'ф', [245] = 'х', [246] = 'ц', [247] = 'ч', [248] = 'ш', [249] = 'щ', [250] = 'ъ', [251] = 'ы', [252] = 'ь', [253] = 'э', [254] = 'ю', [255] = 'я',
+} 
+
+function main()
+	while true do
+		wait(0)
+	end
+end
+
+function functions.string_split(inputstr, sep)
+    if sep == nil then
+            sep = "%s"
+    end
+    local t={} ; i=1
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+            t[i] = str
+            i = i + 1
+    end
+    return t
+end
+
+function functions.getMyNick()
+    local result, id = sampGetPlayerIdByCharHandle(playerPed)
+    if result then
+        local nick = sampGetPlayerNickname(id)
+        return nick
+    end
+end
+
+function functions.getMyId()
+    local result, id = sampGetPlayerIdByCharHandle(playerPed)
+    if result then
+        return id
+    end
+end
+
+function functions.textSplit(str, delim, plain)
+    local tokens, pos, plain = {}, 1, not (plain == false) --[[ delimiter is plain text by default ]]
+    repeat
+        local npos, epos = string.find(str, delim, pos, plain)
+        table.insert(tokens, string.sub(str, pos, npos and npos - 1))
+        pos = epos and epos + 1
+    until not pos
+    return tokens
+end
+
+function functions.playernickname(nick)
+    nick = tostring(nick)
+    local _, myid = sampGetPlayerIdByCharHandle(PLAYER_PED)
+    if nick == sampGetPlayerNickname(myid) then return myid end
+    for i = 0, 1003 do
+      if sampIsPlayerConnected(i) and sampGetPlayerNickname(i) == nick then
+        return i
+      end
+    end
+  end
+
+function functions.string_rlower(s)
+    s = s:lower()
+    local strlen = s:len()
+    if strlen == 0 then return s end
+    s = s:lower()
+    local output = ''
+    for i = 1, strlen do
+        local ch = s:byte(i)
+        if ch >= 192 and ch <= 223 then -- upper russian characters
+            output = output .. russian_characters[ch + 32]
+        elseif ch == 168 then -- Ё
+            output = output .. russian_characters[184]
+        else
+            output = output .. string.char(ch)
+        end
+    end
+    return output
+end
+
+function functions.string_rupper(s)
+    s = s:upper()
+    local strlen = s:len()
+    if strlen == 0 then return s end
+    s = s:upper()
+    local output = ''
+    for i = 1, strlen do
+        local ch = s:byte(i)
+        if ch >= 224 and ch <= 255 then -- lower russian characters
+            output = output .. russian_characters[ch - 32]
+        elseif ch == 184 then -- ё
+            output = output .. russian_characters[168]
+        else
+            output = output .. string.char(ch)
+        end
+    end
+    return output
+end
+
+function functions.join_argb(a, r, g, b)
+    local argb = b  -- b
+    argb = bit.bor(argb, bit.lshift(g, 8))  -- g
+    argb = bit.bor(argb, bit.lshift(r, 16)) -- r
+    argb = bit.bor(argb, bit.lshift(a, 24)) -- a
+    return argb
+end
+
+function functions.explode_argb(argb)
+    local a = bit.band(bit.rshift(argb, 24), 0xFF)
+    local r = bit.band(bit.rshift(argb, 16), 0xFF)
+    local g = bit.band(bit.rshift(argb, 8), 0xFF)
+    local b = bit.band(argb, 0xFF)
+    return a, r, g, b
+end
+
+return functions, functions
