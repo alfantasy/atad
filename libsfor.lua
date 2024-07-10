d32d7f185d7bdce0d1d24b3c3c069e124c6c8e30
alfantasy <71088194+alfantasy@users.noreply.github.com>
2024-07-10 21:12:18 +0400

    Add files via upload

--- libsfor.lua
+++ libsfor.lua
@@ -1,4 +1,5 @@
 local encoding = require 'encoding' -- регистрация кодирования 
+local imgui = require 'mimgui' -- интеграция интерфейса Moon ImGUI
 u8 = encoding.UTF8 -- определение стандартной кодировки
 encoding.default = 'CP1251' -- изменение кодировки на Cyrillic
 -- проверка
@@ -12,6 +13,59 @@ function main()
 	while true do
 		wait(0)
 	end
+end
+
+function functions.TextColoredRGB(text)
+    local style = imgui.GetStyle()
+    local colors = style.Colors
+    local ImVec4 = imgui.ImVec4
+    local explode_argb = function(argb)
+        local a = bit.band(bit.rshift(argb, 24), 0xFF)
+        local r = bit.band(bit.rshift(argb, 16), 0xFF)
+        local g = bit.band(bit.rshift(argb, 8), 0xFF)
+        local b = bit.band(argb, 0xFF)
+        return a, r, g, b
+    end
+    local getcolor = function(color)
+        if color:sub(1, 6):upper() == 'SSSSSS' then
+            local r, g, b = colors[1].x, colors[1].y, colors[1].z
+            local a = tonumber(color:sub(7, 8), 16) or colors[1].w * 255
+            return ImVec4(r, g, b, a / 255)
+        end
+        local color = type(color) == 'string' and tonumber(color, 16) or color
+        if type(color) ~= 'number' then return end
+        local r, g, b, a = explode_argb(color)
+        return imgui.ImVec4(r/255, g/255, b/255, a/255)
+    end
+    local render_text = function(text_)
+        for w in text_:gmatch('[^\r\n]+') do
+            local text, colors_, m = {}, {}, 1
+            w = w:gsub('{(......)}', '{%1FF}')
+            while w:find('{........}') do
+                local n, k = w:find('{........}')
+                local color = getcolor(w:sub(n + 1, k - 1))
+                if color then
+                    text[#text], text[#text + 1] = w:sub(m, n - 1), w:sub(k + 1, #w)
+                    colors_[#colors_ + 1] = color
+                    m = n
+                end
+                w = w:sub(1, n - 1) .. w:sub(k + 1, #w)
+            end
+            if text[0] then
+                for i = 0, #text do
+                    imgui.TextColored(colors_[i] or colors[1], u8(text[i]))
+                    imgui.SameLine(nil, 0)
+                end
+                imgui.NewLine()
+            else imgui.Text(u8(w)) end
+        end
+    end
+    render_text(text)
+end
+
+function functions.CenterText(text)
+    imgui.SetCursorPosX(imgui.GetWindowWidth()/2-imgui.CalcTextSize(u8(text)).x/2)
+    imgui.Text(u8(text))
 end
 
 function functions.string_split(inputstr, sep)
@@ -116,4 +170,12 @@ function functions.explode_argb(argb)
     return a, r, g, b
 end
 
-return functions, functions
+function functions.explode_argbFloat(argb)
+    local a = bit.band(bit.rshift(argb, 24), 0xFF)
+    local r = bit.band(bit.rshift(argb, 16), 0xFF)
+    local g = bit.band(bit.rshift(argb, 8), 0xFF)
+    local b = bit.band(argb, 0xFF)
+    return r, g, b, a
+end
+
+return functions
